b8bcd01db64db2b2b5d9abc6d8e42af07db0faaa
Sai Chebrolu <saivallabha@gmail.com>
2021-03-10 21:58:58 -0800

    Added Comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/LiveAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/LiveAuto.java
@@ -17,6 +17,7 @@ public class LiveAuto extends LiveTeleopBase {
 
     @Override
     public void on_init() {
+        //set intial position of the robot
         robot.drive_train.setInitPos(-15.0, -61.0, Math.PI);
         robot.phone_camera.start_streaming();
     }
@@ -27,26 +28,28 @@ public class LiveAuto extends LiveTeleopBase {
 // I am Sai
 //        robot.wobble.closeWobbleLift(true);
 //        robot.wobble.openWobbleServo(true);
-        robot.wobble.closeWobbleServo(true);
+        robot.wobble.closeWobbleServo(true); // close wobble servo fr fr
 //        robot.wobble.middleWobbleLift(true);
 
-        int pattren = robot.phone_camera.get_pattern();
+        int pattren = robot.phone_camera.get_pattern(); //  get pattern value
         robot.phone_camera.stop_streaming();
 
-        aroundRings(pattren);
-        gotoRectBasedOnPattren(pattren);
-        robot.wobble.dropWobble(true);
-        robot.shooter.spin(true);
+        aroundRings(pattren); // go around rings based on the pattern
+        gotoRectBasedOnPattren(pattren); // move to correct rectangle based on pattern
+        robot.wobble.dropWobble(true); // drop the wobble arm and releasing at same time
+        robot.shooter.spin(true); // starts spinning the green wheel to shoot
 
+         // navigate around just-dropped wobble if it's in first rectangle
         if(pattren==1){
             robot.drive_train.odo_move(robot.drive_train.get_X(), robot.drive_train.get_Y()-5, Math.PI, 1, distance_acc, angle_acc);
             robot.drive_train.odo_move(robot.drive_train.get_X()+5, robot.drive_train.get_Y()-3, Math.PI, 1, distance_acc, angle_acc);
         }
 
+        // moves directly to shooting position and some changes due to some whack stuff
         if(pattren == 3){
             robot.drive_train.odo_move(-15, -3, Math.PI + ((Math.PI/180)*4), 1, distance_acc, angle_acc);
         } else {
-            robot.drive_train.odo_move(-12, -3, Math.PI, 1, distance_acc, angle_acc);
+            robot.drive_train.odo_move(-12, -3, Math.PI+ ((Math.PI/180)*4), 1, distance_acc, angle_acc);
         }
 
 
@@ -54,6 +57,8 @@ public class LiveAuto extends LiveTeleopBase {
         if(!isOpmodeActive()) return;
         //robot.drive_train.odo_move(robot.drive_train.get_X(), robot.drive_train.get_Y(), robot.drive_train.get_A()+ ((Math.PI/180)*4), 1, distance_acc, angle_acc);
         //waitForTime(0.5);
+
+        //shooting 3 rings and subtracting every time to make sure it doesn't go to the over
         robot.shooter.shoot(robot.shooter.getShooterSpped() - 17);
         //sleep(150);
         robot.shooter.shoot(robot.shooter.getShooterSpped() - 48);
@@ -67,15 +72,19 @@ public class LiveAuto extends LiveTeleopBase {
 
         if(pattren > 1){
             elapsedtimer.reset();
+            // collects the rings in th
             intake(pattren);
             //rightBeforeLine();
+
+            // shoots once
             if(pattren == 2){
                 //shootpos(pattren);
-                robot.drive_train.odo_move(-10, -3, Math.PI, 1, distance_acc, angle_acc);
+                robot.drive_train.odo_move(-10, -3, Math.PI + ((Math.PI/180)*4), 1, distance_acc, angle_acc);
                 robot.shooter.shoot();
                 robot.shooter.unshoot();
             }
 
+            // shoots 3 times
             if(pattren == 3){
                 robot.drive_train.odo_move(-14, -3, Math.PI + ((Math.PI/180)*4.5), 1, distance_acc, angle_acc);
                 robot.shooter.shoot();
@@ -86,10 +95,18 @@ public class LiveAuto extends LiveTeleopBase {
 
         }
 
-        secondWobble();
-        gotoRectBasedOnPattren(pattren);
-        robot.wobble.dropWobble(true);
-        gotoLine();
+        secondWobble(); // grab second wobble
+         // navigate to correct rectangle based on pattern
+        if(pattren==1){
+            robot.drive_train.odo_move(-28.0, -12.0, Math.PI, 1, distance_acc, angle_acc);
+        } else if (pattren==2){
+            robot.drive_train.odo_move(-2.0, 12.0, Math.PI, 1, distance_acc, angle_acc);
+        } else if (pattren==3){
+            robot.drive_train.odo_move(-16, 33.0, Math.PI, 1, distance_acc, angle_acc);
+        }
+        //gotoRectBasedOnPattren(pattren);
+        robot.wobble.dropWobble(true); // drop wobble
+        gotoLine(); // go to the white line
 
     }
     private void aroundRings(int pattren) {
@@ -101,9 +118,9 @@ public class LiveAuto extends LiveTeleopBase {
     }
 
     private void secondWobble(){
-        robot.wobble.middleWobbleLift(true);
+        robot.wobble.closeWobbleLift(true);
         robot.wobble.openWobbleServo(true);
-        robot.drive_train.odo_move(-6.0, -36, -0, 1, distance_acc, angle_acc);
+        robot.drive_train.odo_move(-6.0, -40, -0, 1, distance_acc, angle_acc);
         robot.wobble.closeWobbleServo(true);
     }
 
@@ -114,17 +131,21 @@ public class LiveAuto extends LiveTeleopBase {
         robot.drive_train.odo_move(-14.0, -50.0, robot.drive_train.get_A() + ((Math.PI/180)*8), 1, distance_acc, angle_acc);
     }
     private void intake(int pattren){
+
         ElapsedTime elapsedtimer = new ElapsedTime();
+
+        // move to correct x-coordinates based on how many rings are there
         if(pattren == 3){
             robot.drive_train.odo_move(-8.3, 0, Math.PI + ((Math.PI/180)*7), 1, distance_acc, Math.PI/360);
             robot.drive_train.odo_move(robot.drive_train.get_X(), -31.0, robot.drive_train.get_A(), 1, distance_acc, Math.PI / 360);
         }else{
             robot.drive_train.odo_move(-4, 0, robot.drive_train.get_A(), 1, distance_acc, Math.PI/360);
         }
-        robot.intake.setIntakePower(1);
+        robot.intake.setIntakePower(1); // intake starts spinning
         waitForTime(1);
         while(elapsedtimer.seconds()<6.0){
-            robot.intake.setIntakePower(1);
+            robot.intake.setIntakePower(1); // for safety
+            // move the robot vertically while intaking to successfully collect rings
             if(pattren == 3){
                 //robot.drive_train.odo_move(robot.drive_train.get_X(), -25.0, robot.drive_train.get_A(), 1, distance_acc, Math.PI / 360);
                 robot.drive_train.odo_move(robot.drive_train.get_X(), robot.drive_train.get_Y()-1, robot.drive_train.get_A(), 1, distance_acc, Math.PI / 360);
@@ -152,7 +173,7 @@ public class LiveAuto extends LiveTeleopBase {
         robot.drive_train.odo_move(-28.0, -12.0, robot.drive_train.get_A() + (Math.PI/80), 1, distance_acc, angle_acc);
         switch (pattren) {
             case 2:
-                robot.drive_train.odo_move(-2.0, 12.0, robot.drive_train.get_A(), 1, distance_acc, angle_acc);
+                robot.drive_train.odo_move(0, 12.0, robot.drive_train.get_A(), 1, distance_acc, angle_acc);
                 break;
             case 3:
                 robot.drive_train.odo_move(-16, 33.0, robot.drive_train.get_A(), 1, distance_acc, angle_acc);
